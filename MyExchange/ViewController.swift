4db21742b496f67e30e6651bd6f860d6274ea792
masterUNG <phrombutr@gmail.com>
2015-05-28 09:16:19 +0700

    Initial Commit

---
+++ MyExchange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyExchange
+//
+//  Created by masterUNG on 5/28/2558 BE.
+//  Copyright (c) 2558 masterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
